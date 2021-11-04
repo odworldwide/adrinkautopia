47f34437fa2c3783a9ecf656213b7603eb294349
odworldwide <88282766+odworldwide@users.noreply.github.com>
2021-11-04 17:30:12 +0000

    Add files via upload

---
+++ sounds.js
@@ -0,0 +1,25 @@
+import {Howl, Howler} from 'howler';
+
+const sounds = new Howl({
+  "src": [
+    "public\sounds\sounds.webm",
+    "public\sounds\sounds.mp3"
+  ],
+  "sprite": {
+    "jump": [
+      0,
+      783.6734693877551
+    ],
+    "okandi_cmwif_m8.mp3": [
+      2000,
+      259715.78231292518
+    ],
+    "run": [
+      263000,
+      313.4693877551058
+    ]
+  }
+})
+
+sounds.play('okandi_cmwif_m8')()
+
