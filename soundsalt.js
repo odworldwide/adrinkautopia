c0e3c6b403c5f01af896ad74836e3b0d804f48fe
odworldwide <88282766+odworldwide@users.noreply.github.com>
2021-11-04 17:53:04 +0000

    Update soundsalt.js

--- soundsalt.js
+++ soundsalt.js
@@ -1,23 +1,25 @@
-const sounds = new Howl({
-  "src": [
-    "./sounds/sounds.webm",
-    "./sounds/sounds.mp3"
-  ],
-  "sprite": {
-    "jump": [
-      0,
-      783.6734693877551
-    ],
-    "okandi_cmwif_m8": [
-      2000,
-      259715.78231292518
-    ],
-    "run": [
-      263000,
-      313.4693877551058
-    ]
-  }
-})
+;(function () {
 
-sounds.play('okandi_cmwif_m8')
-})()
+  const sounds = new Howl({
+    "src": [
+      "./sounds/sounds.webm",
+      "./sounds/sounds.mp3"
+    ],
+    "sprite": {
+      "jump": [
+        0,
+        783.6734693877551
+      ],
+      "okandi_cmwif_m8": [
+        2000,
+        259715.78231292518
+      ],
+      "run": [
+        263000,
+        313.4693877551058
+      ]
+    }
+  })
+
+  sounds.play('okandi_cmwif_m8')
+  })()
