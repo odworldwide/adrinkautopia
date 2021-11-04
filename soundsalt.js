c99e7c669a58d2b7f797e45f909a2eee5485c2d0
odworldwide <88282766+odworldwide@users.noreply.github.com>
2021-11-04 17:42:54 +0000

    Create soundsalt.js

---
+++ soundsalt.js
@@ -0,0 +1,23 @@
+const sounds = new Howl({
+  "src": [
+    "./sounds/sounds.webm",
+    "./sounds/sounds.mp3"
+  ],
+  "sprite": {
+    "jump": [
+      0,
+      783.6734693877551
+    ],
+    "OKANDI_CMWIF_M8": [
+      2000,
+      259715.78231292518
+    ],
+    "run": [
+      263000,
+      313.4693877551058
+    ]
+  }
+})
+
+sounds.play('OKANDI_CMWIF_M8')
+})()
